dd28526093afe0347b1799989450a46d945dfc84
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-25 15:35:04 +0700

    remake state for counter js

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 const Number = () => {
     return ( 
         <h1>0</h1>
      );
 }
+
+const mapStateToProps = store => ({
+    number : store.counter
+})
  
-export default Number;
+export default connect()(Number);
